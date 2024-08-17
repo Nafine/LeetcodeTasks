70af5941867efc3ed2dda36d950fe80a21cf25aa
Nafine <pshonka_01@mail.ru>
2024-08-17 21:05:09 +0300

    Renamed and deleted some files

---
+++ 9. Palindrome Number/isPalindrome.cpp
@@ -0,0 +1,15 @@
+bool isPalindrome(int x) {
+     
+        int revNum = 0;
+        
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        
+        while(x > revNum)
+        {
+            revNum = revNum * 10 + x % 10;
+            
+            x /= 10;
+        }
+        
+        return x == revNum || x == revNum / 10;
+    }      
